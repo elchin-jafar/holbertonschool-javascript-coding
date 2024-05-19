d1121d5e2d8db0da596bedebfbe5b03815b6e35a
Elchin <elcinceferli@gmail.com>
2024-05-19 23:38:15 +0400

    all

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, res, body) => {
+    if (err) throw err;
+    console.log(JSON.parse(body).title);
+  }
+);
